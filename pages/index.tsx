f9c3794b0e4979b37981df7906f1bbec983107c9
Chanakyha <vetrichanakyha2003@gmail.com>
2022-09-10 17:43:13 +0530

    Get Details from contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
-import { useAddress, useContract } from "@thirdweb-dev/react";
+import { useAddress, useContract, useContractData } from "@thirdweb-dev/react";
 import Login from "../components/Login";
 import Loading from "../components/Loading";
+import { BarLoader } from "react-spinners";
+import { ethers } from "ethers";
+import { currency } from "../constants";
 
 const Home: NextPage = () => {
   const address = useAddress();
@@ -12,6 +15,20 @@ const Home: NextPage = () => {
   const { contract, isLoading } = useContract(
     process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
   );
+
+  const { data: remainingTickets, isLoading: remainingTicketsLoading } =
+    useContractData(contract, "RemainingTickets");
+
+  const { data: currentWinningReward, isLoading: currentWinningRewardLoading } =
+    useContractData(contract, "CurrentWinningReward");
+
+  const { data: ticketPrice, isLoading: ticketPriceLoading } = useContractData(
+    contract,
+    "ticketPrice"
+  );
+
+  const { data: ticketCommission, isLoading: ticketCommissionLoading } =
+    useContractData(contract, "ticketCommission");
 
   if (isLoading) return <Loading />;
   if (!address) return <Login />;
@@ -22,73 +39,111 @@ const Home: NextPage = () => {
         <title>CrypDraw Dapp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="flex-1">
+        <Header />
 
-      <Header />
+        {/* The Next Draw Box */}
+        <div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
+          <div className="stats-container">
+            <h1 className="text-5xl text-white font-semibold text-center">
+              The Next Draw
+            </h1>
+            <div className="flex justify-between p-2 space-x-2">
+              <div className="stats">
+                <h2 className="text-sm">Total Pool</h2>
+                <p className="text-xl">
+                  {currentWinningRewardLoading ? (
+                    <Loader />
+                  ) : (
+                    ethers.utils.formatEther(currentWinningReward.toString()) +
+                    ` ${currency}`
+                  )}
+                </p>
+              </div>
+              <div className="stats">
+                <div className="text-sm">Tickets Remaining</div>
+                <p className="text-xl">
+                  {remainingTicketsLoading ? (
+                    <Loader />
+                  ) : (
+                    remainingTickets?.toNumber()
+                  )}
+                </p>
+              </div>
+            </div>
 
-      {/* The Next Draw Box */}
-      <div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
-        <div className="stats-container">
-          <h1 className="text-5xl text-white font-semibold text-center">
-            The Next Draw
-          </h1>
-          <div className="flex justify-between p-2 space-x-2">
-            <div className="stats">
-              <h2 className="text-sm">Total Pool</h2>
-              <p className="text-xl">0.1 MATIC</p>
-            </div>
-            <div className="stats">
-              <div className="text-sm">Tickets Remaining</div>
-              <p className="text-xl">100</p>
-            </div>
+            {/* Coundown Timer */}
           </div>
 
-          {/* Coundown Timer */}
-        </div>
-
-        <div className="stats-container space-y-2">
-          <div className="stats-containter">
-            <div className="flex justify-between items-center text-white pb-2">
-              <h2>Price per Ticket</h2>
-              <p>0.01 MATIC</p>
-            </div>
-            <div className="flex text-white items-center space-x-2 bg-primary border border-bor-primary p-4">
-              <p>TICKETS</p>
-              <input
-                type="number"
-                min={1}
-                max={10}
-                value={quantity}
-                onChange={(e) => setQuantity(Number(e.target.value))}
-                className="flex w-full bg-transparent text-right outline-none"
-              />
-            </div>
-            <div className="space-y-2 mt-5 ">
-              <div className="flex items-center justify-between text-emerald-300 text-sm italic font-extrabold">
-                <p>Total Cost of Tickets</p>
-                <p>0.999</p>
+          <div className="stats-container space-y-2">
+            <div className="stats-containter">
+              <div className="flex justify-between items-center text-white pb-2">
+                <h2>Price per Ticket</h2>
+                <p>
+                  {ticketPriceLoading ? (
+                    <Loader />
+                  ) : (
+                    ethers.utils.formatEther(ticketPrice.toString()) +
+                    ` ${currency}`
+                  )}
+                </p>
               </div>
-              <div className="flex items-center justify-between text-emerald-300 text-xs italic">
-                <p>Service Fees</p>
-                <p>0.001 MATIC</p>
+              <div className="flex text-white items-center space-x-2 bg-primary border border-bor-primary p-4">
+                <p>TICKETS</p>
+                <input
+                  type="number"
+                  min={1}
+                  max={10}
+                  value={quantity}
+                  onChange={(e) => setQuantity(Number(e.target.value))}
+                  className="flex w-full bg-transparent text-right outline-none"
+                />
               </div>
-              <div className="flex items-center justify-between text-emerald-300 text-xs italic">
-                <p>+ Network Fees</p>
-                <p>TBC</p>
+              <div className="space-y-2 mt-5 ">
+                <div className="flex items-center justify-between text-emerald-300 text-sm italic font-extrabold">
+                  <p>Total Cost of Tickets</p>
+                  <p>
+                    {ticketPriceLoading ? (
+                      <Loader />
+                    ) : (
+                      Number(
+                        ethers.utils.formatEther(ticketPrice?.toString())
+                      ) * quantity
+                    )}
+                  </p>
+                </div>
+                <div className="flex items-center justify-between text-emerald-300 text-xs italic">
+                  <p>Service Fees</p>
+                  <p>
+                    {ticketCommissionLoading ? (
+                      <Loader />
+                    ) : (
+                      ethers.utils.formatEther(ticketCommission.toString()) +
+                      ` ${currency}`
+                    )}
+                  </p>
+                </div>
+                <div className="flex items-center justify-between text-emerald-300 text-xs italic">
+                  <p>+ Network Fees</p>
+                  <p>TBC</p>
+                </div>
               </div>
+              <button className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 disabled:text-gray-100 disabled:cursor-not-allowed">
+                Buy Tickets
+              </button>
             </div>
-            <button className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 disabled:text-gray-100 disabled:cursor-not-allowed">
-              Buy Tickets
-            </button>
           </div>
         </div>
-      </div>
 
-      {/* Price Per Ticket Box */}
-      <div>
-        <div></div>
+        {/* Price Per Ticket Box */}
+        <div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
 };
+
+const Loader = () => <BarLoader color="white" className="mt-2" />;
 
 export default Home;
